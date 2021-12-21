e821e67cbd4180e02d51afac8045ebde3ec43df1
Lyz03 <lizoe@netcourrier.com>
2021-12-21 15:38:02 +0100

    jquery ver

--- app.js
+++ app.js
@@ -5,40 +5,39 @@ const apiKey = "a607699cd5d0d11e490df29ccbc983c8";
  * @param city
  */
 let call = function(city) {
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric&lang=fr";
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric&lang=fr",
+        method: "GET",
+        dataType: "json"
+    })
 
-    fetch(url)
-        .then(response => response.json()
-             .then((data) => {
-                 citySpan.innerText = data.name;
-                 temperatureSpan.innerText = data.main.temp + "°C";
-                 humiditySpan.innerText = data.main.humidity + '%';
-                 windSpan.innerText = Math.round(parseFloat(data.wind.speed) * 3.6).toString()+ "km/h";
-                 descriptionSpan.innerText = data.weather[0].description;
-                 feelsLikeSpan.innerText = data.main.feels_like + "°C";
-            })
-        )
-        .catch((error) => {
-                errorP.innerText = "Une erreur est survenue, pensez a vérifier l'orthographe"
-        } )
+        .done(function (data) {
+            citySpan.text(data.name);
+            temperatureSpan.text(data.main.temp + "°C");
+            humiditySpan.text(data.main.humidity + '%');
+            windSpan.text(Math.round(parseFloat(data.wind.speed) * 3.6).toString()+ "km/h");
+            descriptionSpan.text(data.weather[0].description);
+            feelsLikeSpan.text(data.main.feels_like + "°C");
+        })
+        .fail(function (error) {
+            errorP.text("Une erreur est survenue, pensez a vérifier l'orthographe");
+        })
 }
 
 call("Paris")
 
+const errorP = $("#form p");
 
-const errorP = document.querySelector("#form p");
+const citySpan = $("#city span");
+const temperatureSpan = $("#temperature span");
+const humiditySpan = $("#humidity span");
+const windSpan = $("#wind span");
+const descriptionSpan = $("#description span");
+const feelsLikeSpan = $("#feels_like span");
 
-const citySpan = document.querySelector("#city span");
-const temperatureSpan = document.querySelector("#temperature span");
-const humiditySpan = document.querySelector("#humidity span");
-const windSpan = document.querySelector("#wind span");
-const descriptionSpan = document.querySelector("#description span");
-const feelsLikeSpan = document.querySelector("#feels_like span");
+const submit = $("#form button");
+const input = $("input[type=text]");
 
-const submit = document.querySelector("#form button");
-const input = document.querySelector("input[type=text]");
-
-
-submit.addEventListener("click", (e) => {
-        call(input.value)
+submit.click(function () {
+    call(input.val());
 })
